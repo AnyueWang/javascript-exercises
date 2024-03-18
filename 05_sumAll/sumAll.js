bcdcf686abf0bf25917a4b2a290a349d55bcbd36
AnyueWang <moonwong.way@gmail.com>
2024-03-18 20:13:18 +0100

    Accomplish 05 sumALl

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+    if (numStart < 0 | numEnd < 0) return "ERROR"
+    if (typeof (numStart) !== "number" | typeof (numEnd) !== "number") return "ERROR"
+    let sum = 0;
+    if (numStart > numEnd) {
+        let temp = numStart;
+        numStart = numEnd;
+        numEnd = temp;
+    }
+    for (let i = numStart; i <= numEnd; i++) sum += i;
+    return sum
 };
 
 // Do not edit below this line
