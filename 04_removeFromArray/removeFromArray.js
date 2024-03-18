0174f67ddf9b2af0db8f761e6d23ac2323760602
AnyueWang <moonwong.way@gmail.com>
2024-03-18 19:45:34 +0100

    Accomplish 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    revisedArr = [];
+    i = 0;
+    lenArr = arr.length;
+    while(i < lenArr) {
+        if (! args.includes(arr[i])) {
+            revisedArr.push(arr[i]);
+        }
+        i++;
+    }
+    return revisedArr
 };
 
 // Do not edit below this line
