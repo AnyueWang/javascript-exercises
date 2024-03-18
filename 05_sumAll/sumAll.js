50f6f60dbfb80900d34c640804c8c2c801adee2b
AnyueWang <moonwong.way@gmail.com>
2024-03-18 20:24:18 +0100

    Revise |
    | is for bit-wise operation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function (numStart, numEnd) {
-    if (numStart < 0 | numEnd < 0) return "ERROR"
-    if (typeof (numStart) !== "number" | typeof (numEnd) !== "number") return "ERROR"
+    if (numStart < 0 || numEnd < 0) return "ERROR"
+    if (typeof (numStart) !== "number" || typeof (numEnd) !== "number") return "ERROR"
     let sum = 0;
     if (numStart > numEnd) {
         let temp = numStart;
