6b362d3e3007f6fbd5acf92959cba27b53d4ebf1
AnyueWang <moonwong.way@gmail.com>
2024-03-18 15:39:33 +0100

    Accomplish 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    let lenStr = str.length;
+    let reversedStr = '';
+    for (let i = lenStr; i > 0; i--) {
+        reversedStr += str[i-1];
+    }
+    return reversedStr
 };
 
 // Do not edit below this line
