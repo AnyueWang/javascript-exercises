351e0ee270f9c2cd8f90b778f98639297d68436b
AnyueWang <moonwong.way@gmail.com>
2024-04-04 14:07:05 +0200

    Accomplish 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    people.sort((a, b) => {
+        aYears = getYears(a);
+        bYears = getYears(b);
+        return bYears - aYears
+    });
+    return people[0]
 };
+
+function getYears(person) {
+    return !person.yearOfDeath ? (new Date).getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
