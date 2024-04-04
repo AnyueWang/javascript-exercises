51d7c6624894ee11af098ccb1e265b195b2eb748
AnyueWang <moonwong.way@gmail.com>
2024-04-04 11:45:54 +0200

    Accomplish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strProcessed = str.toLowerCase();
+    let arr = strProcessed.match(/([a-z0-9])/g);
+    for (let i = 0; i < arr.length / 2; i++) {
+        if (arr[i] !== arr[arr.length - i - 1]) return false
+    }
+    return true
 };
 
 // Do not edit below this line
