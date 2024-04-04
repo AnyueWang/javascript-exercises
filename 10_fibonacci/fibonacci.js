ba9832301a1b7c49496955074a77c80a2879ecc9
AnyueWang <moonwong.way@gmail.com>
2024-04-04 12:02:18 +0200

    Accomplish 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const numReal = +num;
+    let first = 1;
+    let second = 1;
+    let i = 0;
+    if (numReal < 0) return "OOPS"
+    if (numReal === 0) return 0
+    if (numReal === 1 || numReal === 2) return 1
+    while (i < numReal-2) {
+        const sum = first + second;
+        first = second;
+        second = sum;
+        i++;
+    }
+    return second
 };
 
 // Do not edit below this line
